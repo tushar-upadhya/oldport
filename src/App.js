b4ef83ee0611cb95ee35de497bd86fd35db2232a
tushar upadhyay <tusharupadhyay690@gmail.com>
2024-06-27 17:35:38 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import TechStack from "./components/TechStack";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Hero />
+
+      <Projects />
+
+      <TechStack />
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
